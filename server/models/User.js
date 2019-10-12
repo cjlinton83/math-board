5cb726b527c9d8c8ada5e7e2fbe80c0b6635c5b3
Chris Linton <cjlinton83@gmail.com>
2019-10-12 13:33:07 -0500

    database connected to server

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    role: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const User = mongoose.model('users', userSchema)
+
+module.exports = User
