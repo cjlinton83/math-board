6c0bcc739a9b65b617f68d0f395485c28b6fbbb4
Chris Linton <cjlinton83@gmail.com>
2019-10-16 04:13:49 -0500

    Revert "another try to fix error"
    This reverts commit 471be41776f91f112c68287f0e3ca3f00c0e132a.

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
-// import { createStore, applyMiddleware, compose } from 'redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
@@ -9,11 +8,10 @@ const middleware = [thunk]
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middleware)
-  // compose(
-  //   applyMiddleware(...middleware),
-  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  // )
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
 
 export default store
