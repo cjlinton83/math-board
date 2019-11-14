02265f449def6092cd69de16406de6c713eb6b6c
Ludvig <ludvig.andersson@selu.edu>
2019-11-14 16:57:37 -0600

    html for password

--- src/components/auth/hello_template.js
+++ src/components/auth/hello_template.js
@@ -4,20 +4,78 @@ const Hello = data => {
       <!DOCTYPE html>
      <html style="margin: 0; padding: 0;">
      
-         <head>
-             <title>Password Reset</title>
-         </head>
-     
-             <body style="margin: 0; padding: 0;">
-                <br />
-                <br />
-                <div>Password reset email </div>
-                <br />
-                <br />
-             </body>
+         	
+<body style="width:100% !important; margin:0px auto; padding:0px 0px 0px 0px; background-color:#EAECED;">
+<table style="width:100% !important; margin:0px auto; padding:0px 0px 0px 0px; background-color:#EAECED;">
+    <tr>
+        <td>
+            
+            
+
+            <!-- === CONTENT === -->
+            <table cellspacing="0" cellpadding="0" style="width:100% !important; background-color:#EAECED; margin:0px; padding:35px 50px 0px 50px;">
+            <tbody>
+
+                <tr>
+                    <td>
+                        <table border="0" cellspacing="0" cellpadding="0" style="width:580px; margin:0 auto; padding:0px 0px 70px 0px; border:0px; border-radius: 5px; background:#fff;">
+                        <tbody>
+                                                        <tr>	
+                                <td colspan="6" style="margin:0px; padding:0px; text-align:center;">
+                                    <img style="width:300px; margin:0px auto; padding:0px; text-align:center; border-top-left-radius:5px; border-top-right-radius:5px;" src="https://postal.churchoftheking.com/uploads/eblasts/Pi-Star.png" alt="Forgot Password">
+                                </td>
+                            </tr>
+                            
+                                                        <tr>	
+                                <td colspan="6" style="margin:0px; padding:45px 60px 0px 60px; font-family: Arial, Helvetica, sans-serif; font-size:30px; line-height:34px; font-weight:600; color:#1B2123; text-align:center;">
+                                    Forgot Password                                </td>
+                            </tr>
+                            
+                            
+                                                        <tr>	
+                                <td colspan="6" style="margin:0px; padding:22px 60px 0px 60px; font-family: Arial, Helvetica, sans-serif; font-size:17px; font-weight:400; line-height:31px;  color:#6A6A6A; text-align:center;">
+                                    <p>Did you just forget your MathBoard password? Don't let it happen again.. Update your profile below.</p>                                </td>
+                            </tr>
+                            
+
+                                                        <tr>	
+                                <td colspan="" style="padding:55px 0px 0px 0px; text-align:center;">
+
+                                                                    <a href="http://localhost:3000/signup" style="margin:0px auto; padding:17px 50px; background:#3DA0CE; border-radius:3px; text-align:center; font-family: Arial, Helvetica, sans-serif; font-size:14px; font-weight:400; line-height:16px; color:#fff; text-transform:uppercase; text-decoration:none; letter-spacing:.05em;">Update Your Profile</a>
+                                
+                                                                </td>
+
+                            </tr>
+                            
+
+                                                    </tbody>
+                        </table>
+                    </td>
+                </tr>
+
+            </tbody>
+            </table> 
+
+
+
+
+            
+            </tbody>
+            </table> 
+
+
+            
+    
+        </td>
+    </tr>
+</table>   
+</body>   
+
+
+
      
        </html>
       `;
-  };
-  
-  module.exports = { Hello };
+};
+
+module.exports = { Hello };
