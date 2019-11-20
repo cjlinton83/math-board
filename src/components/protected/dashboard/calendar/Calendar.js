a35fb2dd0bc52f5377581f8d0ec606288c9ac545
Chris Linton <cjlinton83@gmail.com>
2019-11-20 17:33:16 -0600

    added a few things

--- src/components/protected/dashboard/calendar/Calendar.js
+++ src/components/protected/dashboard/calendar/Calendar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import listPlugin from '@fullcalendar/list'
 import googleCalendarPlugin from '@fullcalendar/google-calendar'
 
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/list/main.css';
 
 import GoogleKeys from '../../../../keys/googleCalendar'
 
@@ -12,9 +14,18 @@ const Calendar = props => {
   return(
     <FullCalendar
       defaultView='dayGridMonth'
-      plugins={[ dayGridPlugin, googleCalendarPlugin ]}
+      plugins={[ dayGridPlugin, listPlugin, googleCalendarPlugin ]}
+      header={{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,listYear'
+      }}
       googleCalendarApiKey={GoogleKeys.googleCalendarApiKey}
       events={{ googleCalendarId: GoogleKeys.googleCalendarId }}
+      eventClick={(arg) => {
+        window.open(arg.event.url, '_blank', 'width=700, height=600')
+        arg.jsEvent.preventDefault();
+      }}
     />
   )
 }
