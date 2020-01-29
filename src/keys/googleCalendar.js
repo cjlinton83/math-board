bad9878cff7c65454f9c447ddbcde2593afac3de
Chris Linton <cjlinton83@gmail.com>
2020-01-29 10:28:11 -0600

    Delete googleCalendar.js (keys)

--- src/keys/googleCalendar.js
+++
@@ -1,6 +0,0 @@
-const GoogleKeys = {
-  googleCalendarApiKey: 'AIzaSyAvJuwuF1u1Dzv2c3vKf3Ptl0ZjSQpwW6Q',
-  googleCalendarId: 'jc2fq3m743enn7eoq3sg47i1s8@group.calendar.google.com'
-}
-
-export default GoogleKeys
